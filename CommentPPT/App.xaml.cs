0e319aaab5ab7609e02ee1e4c231379c0bf9a134
117503445 <t117503445@gmail.com>
2018-07-07 00:01:29 +0800

    Initial commit

---
+++ CommentPPT/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using TLib.Software;
+namespace CommentPPT
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string Dir_APP { get; set; } = AppDomain.CurrentDomain.BaseDirectory;
+        public static string Dir_File { get; set; } = Dir_APP + "File/";
+        
+        public static string dir_LstPaths = Dir_File + "LstPaths.xml";
+        public static BindingList<string> LstPaths { get; set; }
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Directory.CreateDirectory(Dir_File);
+
+
+            if (File.Exists(dir_LstPaths))
+            {
+                LstPaths = SerializeHelper.Load <BindingList<string> > (dir_LstPaths);
+            }
+            else
+            {
+                LstPaths = new BindingList<string>();
+            }
+        }
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            SerializeHelper.Save(LstPaths, dir_LstPaths);
+        }
+
+
+    }
+}
---
+++ CommentPPT/WndStart.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using TLib.Software;
+namespace CommentPPT
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class WndStart : Window
+    {
+        public WndStart()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = App.LstPaths.Count-1; i>=0; i--)
+            {
+                if (!File.Exists(App.LstPaths[i]))
+                {
+                    App.LstPaths.RemoveAt(i);
+                }
+            }
+            LstPPT.ItemsSource = App.LstPaths;
+        }
+        private void BtnExplorer_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog()
+            {
+                Filter = "PowerPoint Presentations|*.ppt;*.pptx"
+            };
+            var result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                if (!App.LstPaths.Contains(openFileDialog.FileName))
+                {
+                    App.LstPaths.Add(openFileDialog.FileName);
+                }
+                OpenPPT(openFileDialog.FileName);
+            }
+        }
+        /// <summary>
+        /// 使用ppt打开指定path的文件
+        /// </summary>
+        /// <param name="path"></param>
+        private void OpenPPT(string path)
+        {
+            var index = App.LstPaths.IndexOf(path);
+            var temp = App.LstPaths[0];
+            App.LstPaths[0] = App.LstPaths[index];
+            App.LstPaths[index] = temp;
+
+            System.Diagnostics.Process.Start(path);
+        }
+
+        private void LstPPT_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            OpenPPT((string)LstPPT.SelectedItem);
+            
+        }
+    }
+}
